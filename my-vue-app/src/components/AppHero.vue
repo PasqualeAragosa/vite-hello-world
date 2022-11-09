1caf9f5fd1d6e8662bfa8460b41cbc8fc6be90f4
Pasquale Aragosa <aragosap@gmail.com>
2022-11-09 15:36:00 +0100

    add bonus

---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,14 @@
+<script></script>
+
+<template>
+    <div class="jumbotron">
+        <img src="../assets/img/download.jpeg" alt="">
+    </div>
+    <!-- /.jumbotron -->
+</template>
+
+<style>
+    img {
+        max-width: 100%;
+    }
+</style>
---
+++ my-vue-app/src/components/AppTitle.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+    <h1>La mia prima app Vite!</h1>
+</template>
+
+<style>
+
+</style>
